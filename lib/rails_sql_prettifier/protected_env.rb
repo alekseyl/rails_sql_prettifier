3f4690d288dc4a804272191ef413d4949fe78203
alekseyl <leshchuk@gmail.com>
2025-02-22 20:05:05 +0300

    fixed issue for rails 7.2

--- lib/rails_sql_prettifier/protected_env.rb
+++ lib/rails_sql_prettifier/protected_env.rb
@@ -4,7 +4,11 @@ module RailsSQLPrettifier
 
   module ProtectedEnv
     def protected_env?
-      ActiveRecord::Base.connection.migration_context.protected_environment? ||
+
+      migration_context = ( ActiveRecord::Base.connection.try(:migration_context) ||
+        ActiveRecord::Base.connection.try(:pool)&.migration_context ) # rails 7.2+
+
+      migration_context&.protected_environment? ||
         defined?(Rails) && !(Rails.env.test? || Rails.env.development?)
     end
   end
